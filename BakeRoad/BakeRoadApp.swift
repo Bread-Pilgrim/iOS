7aab83bb812fbf480182cef795e077a2e7ee0706
Hyunho <ho971122@gmail.com>
2025-06-05 12:10:24 +0900

    Initial Commit

---
+++ BakeRoad/BakeRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeRoadApp.swift
+//  BakeRoad
+//
+//  Created by 이현호 on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BakeRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
