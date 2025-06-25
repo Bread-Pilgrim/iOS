8bf2317c4a72a830ce0c5b70b5dc6925acd687fc
Hyunho <ho971122@gmail.com>
2025-06-25 12:25:02 +0900

    feat(카카오 로그인): 카카오 로그인 관련 sdk import 및 로직 추가(테스트)

--- BakeRoad/BakeRoadApp.swift
+++ BakeRoad/BakeRoadApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct BakeRoadApp: App {
+    init() {
+        KakaoSDK.initSDK(appKey: "51502a87adb7922bc9b5e1fe7cc39c5a")
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // onOpenURL()을 사용해 커스텀 URL 스킴 처리
+            ContentView().onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    AuthController.handleOpenUrl(url: url)
+                }
+            })
         }
     }
 }
--- BakeRoad/ContentView.swift
+++ BakeRoad/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+import KakaoSDKUser
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Button("", image: ImageResource(name: "kakao_login_medium_narrow", bundle: .main)) {
+                KakaoLogin()
+            }
         }
         .padding()
+    }
+    
+    func kakaoLonginWithApp() {
+        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoTalk() success.")
+                
+                //do something
+                _ = oauthToken
+            }
+        }
+    }
+    
+    func kakaoLoginWithAccount() {
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+                
+                //do something
+                _ = oauthToken
+            }
+        }
+    }
+    
+    func KakaoLogin() {
+        // 카카오톡 실행 가능 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            // 카카오톡 앱으로 로그인 인증
+            kakaoLonginWithApp()
+        } else { // 카톡이 설치가 안 되어 있을 때
+            // 카카오 계정으로 로그인
+            kakaoLoginWithAccount()
+        }
     }
 }
 
