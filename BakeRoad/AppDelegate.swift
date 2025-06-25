ebfd98f436c509c7ba9fa33e02ce8d4685df58ac
Hyunho <ho971122@gmail.com>
2025-06-25 12:20:49 +0900

    feat: AppDelegate 파일 추가 및 카카오 로그인 세팅

---
+++ BakeRoad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BakeRoad
+//
+//  Created by 이현호 on 6/18/25.
+//
+
+import UIKit
+
+import KakaoSDKAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        return false
+    }
+}
