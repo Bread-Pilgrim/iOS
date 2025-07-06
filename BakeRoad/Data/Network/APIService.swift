5340bac899926b901cd89f24b8c541639f22e68c
skyler <ho971122@gmail.com>
2025-07-06 21:38:33 +0900

    feat: APIService 네트워크 공통함수 구현

--- BakeRoad/Data/Network/APIService.swift
+++ BakeRoad/Data/Network/APIService.swift
@@ -7,14 +7,46 @@
 
 import Alamofire
 
-public final class APIService {
-    public static let shared = APIService()
+final class APIService {
+    static let shared = APIService() // 또는 DI로 관리
 
+    private let baseURL = "https://fastapi-1015297428835.asia-northeast3.run.app"
     private let session: Session
-    private let baseURL: String = AppConstant.baseURL
 
     private init() {
         let interceptor = AuthInterceptor()
-        session = Session(interceptor: interceptor)
+        self.session = Session(interceptor: interceptor)
+    }
+
+    func request<T: Decodable>(
+        _ request: APIRequest,
+        responseType: T.Type
+    ) async throws -> T {
+        let url = baseURL + request.path
+        let method = HTTPMethod(rawValue: request.method.rawValue)
+
+        let parameters = try request.parameters?.asDictionary()
+
+        let dataTask = session.request(
+            url,
+            method: method,
+            parameters: parameters,
+            encoding: request.method == .get ? URLEncoding.default : JSONEncoding.default,
+            headers: ["Content-Type": "application/json"]
+        )
+        .validate()
+
+        let data = try await withCheckedThrowingContinuation { continuation in
+            dataTask.responseDecodable(of: T.self) { response in
+                switch response.result {
+                case .success(let result):
+                    continuation.resume(returning: result)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+
+        return data
     }
 }
