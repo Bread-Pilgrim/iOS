cff198426ca499aac50c8f8ac80e2f093269d493
skyler <ho971122@gmail.com>
2025-07-06 22:27:51 +0900

    feat: Auth API 관련 Path 상수화

---
+++ BakeRoad/Data/Network/APIPath/AuthPath.swift
@@ -0,0 +1,13 @@
+//
+//  AuthPath.swift
+//  BakeRoad
+//
+//  Created by 이현호 on 7/6/25.
+//
+
+import Foundation
+
+enum AuthPath {
+    static let login = "/auth/login"
+    static let verifyToken = "/auth/token/verify"
+}
