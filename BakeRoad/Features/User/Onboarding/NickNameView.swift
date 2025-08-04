7b12dd313a493b5016e1ea9eb1864e8cef1af81a
Hyunho <ho971122@gmail.com>
2025-08-04 21:30:25 +0900

    feat: 닉네임 설정 화면 생성

---
+++ BakeRoad/Features/User/Onboarding/NickNameView.swift
@@ -0,0 +1,88 @@
+//
+//  NickNameView.swift
+//  BakeRoad
+//
+//  Created by 이현호 on 8/4/25.
+//
+
+import SwiftUI
+
+struct NickNameView: View {    
+    var body: some View {
+        NavigationStack {
+            NickNameSetView()
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            // 뒤로가기 또는 커스텀 액션
+                        }) {
+                            Image("arrowLeft")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                        }
+                    }
+                }
+        }
+    }
+}
+
+struct NickNameSetView: View {
+    @State private var nickname: String = ""
+    @State private var isCheckMsg: String = ""
+    
+    private var isValid: Bool {
+        nickname.trimmingCharacters(in: .whitespaces).isEmpty ||
+        nickname.trimmingCharacters(in: .whitespaces).count > 8
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 6) {
+                Text("회원님의 닉네임을 설정해주세요.")
+                    .font(.headingLargeBold)
+                    .foregroundColor(.black)
+                Text("취향이 담긴 닉네임을 추천드려요!")
+                    .font(.bodySmallRegular)
+                    .foregroundColor(.gray800)
+            }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("닉네임")
+                    .font(.bodyXsmallRegular)
+                    .foregroundColor(.gray990)
+                
+                BakeRoadTextField(title: "",
+                                  placeholder: "빵글이에용",
+                                  description: "nil",
+                                  showTitle: false,
+                                  showDescription: false,
+                                  isEssential: false,
+                                  text: $nickname)
+                .padding(.bottom, 4)
+                
+                if !isCheckMsg.isEmpty {
+                    Text(isCheckMsg)
+                        .font(.body2xsmallRegular)
+                        .foregroundColor(.error400)
+                }
+            }
+            .padding(.top, 44)
+            
+            Spacer()
+            
+            BakeRoadSolidButton(title: "빵글 시작하기",
+                                style: .primary,
+                                size: .xlarge,
+                                isDisabled: isValid) {
+                print("기릿")
+            }
+        }
+        .padding(.top, 11)
+        .padding(.bottom, 24)
+        .padding(.horizontal, 16)
+    }
+}
+
+#Preview {
+    NickNameView()
+}
