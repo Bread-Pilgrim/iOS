6fdcf5d6d83e77c0a4c39ccb96ba95f03632a36b
Hyunho <ho971122@gmail.com>
2025-08-18 17:45:47 +0900

    feat: 애플 로그인 버튼 추가(디자인 확인 및 임시)

--- BakeRoad/Features/Auth/Login/LoginView.swift
+++ BakeRoad/Features/Auth/Login/LoginView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct LoginView: View {
     @StateObject private var viewModel: LoginViewModel
@@ -15,11 +16,18 @@ struct LoginView: View {
     }
     
     var body: some View {
+        SignInWithAppleButton { requst in
+            requst.requestedScopes = [.fullName, .email]
+        } onCompletion: { result in
+            print(result)
+        }
+        
         VStack(spacing: 68) {
             Image("login")
             
             Button {
                 viewModel.login()
+                print("카카오 로그인")
             } label: {
                 Image("kakaoLogin")
             }
@@ -27,3 +35,38 @@ struct LoginView: View {
         .padding()
     }
 }
+
+struct ExampleView: View {
+    var body: some View {
+        VStack {
+            VStack(spacing: 68) {
+                Image("login")
+                
+                VStack(spacing: 10) {
+                    SignInWithAppleButton { requst in
+                        requst.requestedScopes = [.fullName, .email]
+                    } onCompletion: { result in
+                        print(result)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal, 16)
+                    .cornerRadius(10)
+                    
+                    Button {
+                        print("카카오 로그인")
+                    } label: {
+                        Image("kakaoLogin")
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal, 16)
+                    .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview(body: {
+    ExampleView()
+})
