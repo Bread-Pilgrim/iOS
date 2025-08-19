b57f6e96a7371d89d4aba8dee47f2310a0d6ff98
Hyunho <ho971122@gmail.com>
2025-08-19 20:23:57 +0900

    feat: 리뷰 작성 완료 화면 생성

---
+++ BakeRoad/Features/Bakery/Review/ReviewCompleteView.swift
@@ -0,0 +1,45 @@
+//
+//  ReviewCompleteView.swift
+//  BakeRoad
+//
+//  Created by 이현호 on 8/19/25.
+//
+
+import SwiftUI
+
+struct ReviewCompleteView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            
+            VStack(alignment: .center, spacing: 4) {
+                Text("리뷰를 작성했어요!")
+                    .font(.headingMediumBold)
+                    .foregroundColor(.primary500)
+                
+                Text("소중한 경험을 남겨주셔서 감사합니다 🍞")
+                    .font(.bodyMediumSemibold)
+                    .foregroundColor(.black)
+            }
+            .padding(.horizontal, 61)
+            
+            Spacer()
+            
+            HStack(spacing: 8) {
+                BakeRoadSolidButton(title: "홈으로", style: .secondary, size: .large) {
+                    print("홈탭")
+                }
+                
+                BakeRoadSolidButton(title: "리뷰 보기", style: .primary, size: .large) {
+                    print("홈탭")
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 24)
+        }
+    }
+}
+
+#Preview {
+    ReviewCompleteView()
+}
